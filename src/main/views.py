1277f3d5af885812600443d44c1a5e65340039d8
Alerion <alerion.um@gmail.com>
2011-02-10 20:31:42 +0200

    fix settings

--- src/main/views.py
+++ src/main/views.py
@@ -5,6 +5,6 @@ from django.conf import settings
 @login_required
 def index(request):
     context = {
-        'PROJECTS_ON_PAGE': settings.PROJECTS_ON_PAGE
+        'PROJECTS_ON_PAGE': getattr(settings, 'PROJECTS_ON_PAGE', 10)
     }
     return direct_to_template(request, 'main/index.html', context)
